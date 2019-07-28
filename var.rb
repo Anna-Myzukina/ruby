dd15fca279d4494a7023461c7349559bda921c0d
Anna Myzukina <anna.muzykina83@gmail.com>
2019-07-28 20:57:10 +0300

    methods

--- var.rb
+++ var.rb
@@ -1,18 +1,9 @@
 var = 9
 puts var
 str3= %q[some text \n\n\n\n]
-str4 = %Q[some text \n\n\n\n]
-str5 = String.new('Some text')
-puts str3, str4, str5
- 
-i = 10
-while i < 50
-    puts "i: #{i}"
-    i+= 10
-end
 
-i = 5
-until i == 0
-    puts "You can divide number #{i}"
-    i -= 1
+
+def sum(a, b)
+    puts "#{a} + #{b} = #{a + b}"
 end
+sum(1, 9)
