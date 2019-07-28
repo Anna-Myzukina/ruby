983c8b7de561fa30fb58625be36a45dc17d68bfa
Anna Myzukina <anna.muzykina83@gmail.com>
2019-07-28 21:00:03 +0300

    syntaxis methods

---
+++ methods.rb
@@ -0,0 +1,4 @@
+def sum(a, b)
+    puts "#{a} + #{b} = #{a + b}"
+end
+sum(1, 9)
--- var.rb
+++ var.rb
@@ -1,9 +1,19 @@
 var = 9
 puts var
 str3= %q[some text \n\n\n\n]
-
-
-def sum(a, b)
-    puts "#{a} + #{b} = #{a + b}"
+str4 = %Q[some text \n\n\n\n]
+str5 = String.new('Some text')
+puts str3, str4, str5
+ 
+i = 10
+while i < 50
+    puts "i: #{i}"
+    i+= 10
 end
-sum(1, 9)
+
+i = 5
+until i == 0
+    puts "You can divide number #{i}"
+    i -= 1
+end
+
