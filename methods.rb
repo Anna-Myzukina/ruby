dc9e5cb27ac68d4897cb9ae0022dc500ceb4ee16
Anna Myzukina <anna.muzykina83@gmail.com>
2019-08-02 11:34:03 +0300

    add methods for return arguments

--- methods.rb
+++ methods.rb
@@ -1,4 +1,24 @@
+=begin
+Returning Values
+
+You can return multiple values from a method by separating them with commas in the return statement.
+For example:
+def squares(a, b, c)
+  return a*a, b*b, c*c
+end
+
+arr = squares(2, 3, 6)
+puts arr
+=end
+
 def sum(a, b)
     puts "#{a} + #{b} = #{a + b}"
 end
-sum(1, 9)
+puts sum(1, 9)
+
+def squares(x, y, z)
+    return x*x, y*y, z*z
+end
+
+arr = squares(2, 3, 4)
+puts arr[0..1]
